681c8d402d599812aa3878f1f580da391faf20bc
kone-yaya-dev <64926396+kone-yaya-dev@users.noreply.github.com>
2023-01-30 10:32:39 +0000

    first commit

---
+++ Exercise_3_Print_Two_PHP_Variables_Using_Single_Echo/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $variable_1 = "Good Morning"
+    ?>
+    <?php
+    $variable_2 = "Have a nice day!"
+    ?>
+    <h1>
+        <?php
+      echo($variable_1 + $variable_2);
+    ?>
+    </h1>
+</body>
+
+</html>
